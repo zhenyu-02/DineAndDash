52917fc353bf5f06b246e92af455d040279fa46f
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-12-16 10:28:05 +0800

    关注与个人主页展示

--- hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.db.Page;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.entity.UserInfo;
+import com.hmdp.service.IBlogService;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +38,8 @@ public class UserController {
 
     @Resource
     private IUserInfoService userInfoService;
+    @Resource
+    private IBlogService blogService;
 
     /**
      * 发送手机验证码
@@ -87,4 +94,16 @@ public class UserController {
         // 返回
         return Result.ok(info);
     }
+
+    @GetMapping("/{id}")
+    public Result queryUserById(@PathVariable("id") Long userId){
+        User user = userService.getById(userId);
+        if (user == null) {
+            return Result.ok();
+        }
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        return Result.ok(userDTO);
+    }
+
+
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+
+        // 1.判断是关注还是取关
+        if (isFollow) {
+            // 2.关注，新增数据
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+            if (isSuccess) {
+                return Result.ok();
+            }
+            return Result.fail("关注失败");
+        } else {
+            // 3.取关，删除数据
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId)
+            );
+            if (isSuccess) {
+                return Result.ok();
+            }
+            return Result.fail("取关失败");
+        }
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1.获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        // 2.判断是否关注
+        return Result.ok(count > 0);
+    }
 }
